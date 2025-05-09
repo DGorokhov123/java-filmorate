ec20fa9c65efefbe8e627eeca2dc0883a264f6a3
Dmitriy Gorokhov <dgorokhov123@gmail.com>
2025-05-09 21:09:23 +0300

    Issue #11 : added DB table EVENTS + Event storage + Rowmapper

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/EventRowMapper.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.storage.mappers;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.model.enums.EventType;
+import ru.yandex.practicum.filmorate.model.enums.OperationType;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+
+public class EventRowMapper implements RowMapper<Event> {
+
+    public static String GET_FEED_BY_USER_ID_QUERY = """
+            SELECT
+                e.event_id,
+                e.user_id,
+                e.entity_id,
+                e.event_type,
+                e.operation,
+                e.created_at
+            FROM events AS e
+            JOIN friends AS f ON f.friend_id = e.user_id
+            WHERE f.user_id = ?
+            ORDER BY e.event_id DESC;
+            """;
+
+    public static String CREATE_EVENT_QUERY = """
+            INSERT INTO events (user_id, entity_id, event_type, operation, created_at)
+            VALUES (?, ?, ?, ?, ?);
+            """;
+
+
+    @Override
+    public Event mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Event event = new Event();
+        event.setId(rs.getLong("event_id"));
+        event.setUserId(rs.getLong("user_id"));
+        event.setEntityId(rs.getLong("entity_id"));
+        event.setEventType(EventType.valueOf(rs.getString("event_type")));
+        event.setOperation(OperationType.valueOf(rs.getString("operation")));
+        event.setTimestamp(rs.getObject("created_at", OffsetDateTime.class));
+        return event;
+    }
+
+}
